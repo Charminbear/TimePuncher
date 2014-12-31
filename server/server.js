c3c871927b7108c46ee4b666e3ee822e26dc9ec7
Dave <david.losert@gmx.de>
2014-12-31 15:02:33 +0100

    Did a little clean up and replaced routing middleware

--- server/server.js
+++ server/server.js
@@ -3,18 +3,45 @@
  * Created by David on 26.12.14.
  */
 
-const koa = require('koa'),
-	mount = require('koa-mount'),
-	Router = require('koa-router'),
-	betterBody = require('koa-better-body'),
-	co = require('co');
-
 const PORT = process.env.PORT || 3000,
 	ENV = process.env.NODE_ENV || 'development';
 
-var app = module.exports = koa();
+const koa = require('koa'),
+	mount = require('koa-mount'),
+	betterBody = require('koa-better-body'),
+	co = require('co');
 
+var app = module.exports = koa();
 var db = require('./db');
+var router = require('./routes');
+
+connectToDatabase();
+
+// CONFIGURATION & MIDDLEWARE
+app.use(betterBody());
+app.use(function *(next) {
+	var start = new Date;
+	yield next;
+	var ms = new Date - start;
+	console.log('%s %s - %s ms', this.method, this.url, ms);
+});
+
+app.use(mount(router));
+
+// ERROR HANDLING
+app.use(function *() {
+	console.log('Ohoh... I 404ed...');
+	this.throw(404);
+});
+
+
+// START APP
+if (!module.parent) {
+	app.listen(PORT);
+	console.log('App Listening on Port ' + PORT + ' in ' + ENV + ' mode!');
+}
+
+// CONVENIENCE FUNCTIONS
 
 function connectToDatabase () {
 	co(function *() {
@@ -28,38 +55,4 @@ function connectToDatabase () {
 	}).catch(function (err) {
 		console.log('caught error: ', err);
 	});
-}
-connectToDatabase();
-
-app.use(betterBody());
-
-
-// CONFIGURATION
-app.use(function *(next) {
-	var start = new Date;
-	yield next;
-	var ms = new Date - start;
-	console.log('%s %s - %s ms', this.method, this.url, ms);
-});
-
-
-// ROUTES
-var router = new Router();
-router.get('/', function *() {
-	this.body = 'Hello World!';
-});
-
-app.use(mount(router.middleware()));
-
-// ERROR HANDLING
-app.use(function *() {
-	console.log('Ohoh... I 404ed...');
-	this.throw(404);
-});
-
-
-// START APP
-if (!module.parent) {
-	app.listen(PORT);
-	console.log('App Listening on Port ' + PORT + ' in ' + ENV + ' mode!');
 }
